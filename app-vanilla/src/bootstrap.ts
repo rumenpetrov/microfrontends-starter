e4cc881c667f2fa62b7edec87f2150c8e1251aa8
Rumen Petrov <rumenpetrow@gmail.com>
2024-05-21 12:13:22 +0300

    Add vanilla app embed

---
+++ app-vanilla/src/bootstrap.ts
@@ -0,0 +1,43 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+export const bootstrap = (rootElement: HTMLDivElement) => {
+  const rootElementApp = document.createElement('div');
+  rootElementApp.setAttribute('id', 'root');
+  rootElement.appendChild(rootElementApp);
+
+  rootElementApp!.innerHTML = `
+  <div>
+    <div>
+      <p><a href="/app-vanilla">Go to App vanilla page</a></p>
+      <button type="button" onclick="this.nextElementSibling.showModal()">Click me</button>
+      <dialog id="dialog" style="border-color: lime; width: 60vw;">
+        <p>Greetings from within App vanilla!</p>
+        <form method="dialog">
+          <button>OK</button>
+        </form>
+      </dialog>
+    </div>
+
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${new URL(viteLogo, import.meta.url).href}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+    <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+    Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+  setupCounter(rootElementApp.querySelector<HTMLButtonElement>('#counter')!)
+}
+
+export default bootstrap;
---
+++ app-vanilla/vite.config.ts
@@ -0,0 +1,58 @@
+import { defineConfig } from 'vite'
+// import { peerDependencies } from './package.json';
+import cssInject from 'vite-plugin-css-injected-by-js';
+
+export default defineConfig({
+  preview: {
+    port: 9100,
+  },
+  build: {
+    lib: {
+      entry: './src/bootstrap.ts',
+      fileName: 'bundle',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // Defines external dependencies that shouldn't be bundled with your library
+      // external: [...Object.keys(peerDependencies)],
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [
+    cssInject({
+      injectCodeFunction,
+    }),
+  ],
+})
+
+function injectCodeFunction(cssCode: string, options: InjectCodeOptions) {
+  try {
+    if (typeof document == 'undefined' || document.readyState !== 'complete') {
+      setTimeout(() => injectCodeFunction(cssCode, options), 10);
+      return undefined;
+    }
+
+    const hostElementList = document.querySelectorAll('[data-embedded-app-name="app-vanilla"]');
+
+    if (!hostElementList || hostElementList.length < 1) {
+      return undefined;
+    }
+
+    hostElementList.forEach((hostElement) => {
+      const rootElement = hostElement.shadowRoot;
+      const sheet = new CSSStyleSheet();
+      sheet.replaceSync(cssCode);
+
+      if (rootElement) {
+        rootElement.adoptedStyleSheets = [sheet];
+      } else {
+        throw new Error('Missing rootElement.');
+      }
+    });
+  } catch (e) {
+    console.error('vite-plugin-css-injected-by-js', e);
+  }
+
+  return undefined;
+}
